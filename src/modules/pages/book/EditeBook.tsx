a8cd9f9ef599c0cde795f5b1a9421af25fe8fc1c
abdul_jabbar_dev <abdul.jabbar.dev@gmail.com>
2023-07-27 23:49:25 +0600

    edite option created

--- src/modules/pages/book/EditeBook.tsx
+++ src/modules/pages/book/EditeBook.tsx
@@ -1,18 +1,26 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useParams } from "react-router-dom"
 import IBooks from "../../../types/books";
-
+import { useEffect, useState } from 'react';
 
 export default function EditeBook() {
     const { id } = useParams()
-    const { register, handleSubmit, formState: { errors } } = useForm<IBooks>();
+    const [book, setBook] = useState<IBooks>()
+    const { register, handleSubmit, formState: { errors } } = useForm<IBooks>({
+        values: book
+    });
     const onSubmit: SubmitHandler<IBooks> = (data) => {
         console.log(data)
     };
+    useEffect(() => {
+        fetch('/books.json').then(res => res.json()).then((data: IBooks[]) => setBook(data.find(book => book.title === id)!))
+    }, [id])
+
     return (
-        <div className="container mx-auto">
-            <div className="">
-                <form onSubmit={handleSubmit(onSubmit)} className="w-1/2">
+        <div className="container mx-auto ">
+            <div className="flex justify-center my-12">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-1/3">
+                    <h1 className="text-center mb-4 mt-2 text-xl">Let's Edite <span className="font-bold">{book?.title}</span></h1>
                     <div className="w-full">
                         <div className="flex flex-col">
                             <label htmlFor="title">Title</label>
@@ -35,6 +43,7 @@ export default function EditeBook() {
                             {errors.imageURL && <span className="text-xs mt-1 mb-2 text-red-600 ">This field is required</span>}
                         </div>
                     </div>
+                    <input className="border block mx-auto px-3 py-1 rounded-lg uppercase my-5" type="submit" value={'Update'} />
                 </form>
             </div>
         </div>
