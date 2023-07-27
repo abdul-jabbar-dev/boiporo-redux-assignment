97617ef3e8c92aa2b664852b3602b5e51c1311aa
abdul_jabbar_dev <abdul.jabbar.dev@gmail.com>
2023-07-28 02:39:40 +0600

    config stoore

---
+++ src/redux/reduxHooks.ts
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import store from "./store";
+
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.getState>
+> = useSelector;
